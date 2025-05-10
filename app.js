5aae67661355a88920e76d1ac77400b6a105dd06
harshithraihars <harshithraiharsu@gmail.com>
2025-05-10 15:23:52 +0530

    commit 3

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-
+const {router}=require("./route")
 const express=require("express")
 
 const app=express()
-
-app.get("/",(req,res)=>{
-    return res.send("hello")
-})
-
-app.get("/numbers/:numberid",(req,res)=>{
-    console.log();
-    return res.json("")
-})
+app.use("/",router)
 
 app.listen(3000,()=>{
+    
     console.log("listeing tothe port 3000");
     
 })
--- route.js
+++ route.js
@@ -1,7 +1,42 @@
-const express=require("express")
-const app=express()
-app.get("/",(req,res)=>{
-    return res.send("hello")
-})
+const express = require("express");
+const router = express.Router();
 
-module.exports=router
+router.get("/numbers/:numberid", (req, res) => {
+  const numberid = req.params.numberid;
+  
+//   if(numberid=='e'){
+//       console.log("e");
+//   }else if(numberid=='p'){
+//       console.log("p");
+
+//   }
+//   else if(numberid=='r'){
+//       console.log("r");
+
+//   }
+//   else if(numberid=='f'){
+//       console.log("f");
+
+//   }
+  switch (numberid) {
+    case "p":
+      console.log("p");
+
+      break;
+    case "f":
+      console.log("f");
+
+      break;
+    case "e":
+      console.log("e");
+
+      break;
+    case "r":
+      console.log("r");
+
+      break;
+  }
+  return res.json("");
+});
+
+module.exports = { router };
